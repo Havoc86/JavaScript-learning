504c2871a0399afea70f6feedbfceb1f0a196c30
Havoc86 <63926720+Havoc86@users.noreply.github.com>
2020-06-09 01:40:20 +0300

    else if

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-document.body.innerHTML = "This is some content";
-var a = 10
-console.log(a)
+var age = 24
+var gender = "Female"
+
+
+
+if (age >= 18) {
+  if (gender == "Male") {
+    console.log("You're "+ age +" and above the age limit, you're also a dude")
+  } else if (gender == "Female") {
+    console.log("You're "+ age +" and above the age limit, you're also a chick")
+  } else {
+    console.log("You're "+ age +" and above the age limit, but what the fuck are you?")
+  }
+}
+if (age <= 18) {
+  if (gender == "Male") {
+    console.log("You're "+ age +" and below the age limit dude")
+  } else if (gender == "Female") {
+    console.log("You're "+ age +" and below the age limit, you're also a chick, it's ok if you're Clinton")
+  } else {
+    console.log("You're an unidentified "+ age +" year old minor")
+  }
+}
