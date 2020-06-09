15730b942e95a0005e1a4f7eb4160d465da77a16
Havoc86 <63926720+Havoc86@users.noreply.github.com>
2020-06-09 22:40:58 +0300

    Calculator

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-var age = 24
-var gender = "Female"
+function calc() {
+  var a = parseInt(document.querySelector("#value1").value);
+  var b = parseInt(document.querySelector("#value2").value);
+  var op = (document.querySelector("#operator").value);
+  var calculate;
 
-
-
-if (age >= 18) {
-  if (gender == "Male") {
-    console.log("You're "+ age +" and above the age limit, you're also a dude")
-  } else if (gender == "Female") {
-    console.log("You're "+ age +" and above the age limit, you're also a chick")
-  } else {
-    console.log("You're "+ age +" and above the age limit, but what the fuck are you?")
+  if (op == "add") {
+    calculate = a + b;
+  } else if (op == "min") {
+    calculate = a - b;
+  } else if (op == "div") {
+    calculate = a / b;
+  } else if (op == "mul") {
+    calculate = a * b;
   }
-}
-if (age <= 18) {
-  if (gender == "Male") {
-    console.log("You're "+ age +" and below the age limit dude")
-  } else if (gender == "Female") {
-    console.log("You're "+ age +" and below the age limit, you're also a chick, it's ok if you're Clinton")
-  } else {
-    console.log("You're an unidentified "+ age +" year old minor")
-  }
+
+  document.querySelector("#result").innerHTML = calculate;
 }
